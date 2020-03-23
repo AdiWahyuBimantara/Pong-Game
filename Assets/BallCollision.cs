c8cb7961e7f0c7e65c843ad7ff5684b92e7b2fda
AdiWahyuBimantara <18523277@students.uii.ac.id>
2020-03-23 16:23:19 +0700

    merubah code

--- Assets/BallCollision.cs
+++ Assets/BallCollision.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class BallCollision : MonoBehaviour
 {
     // Start is called before the first frame update
+    
+    public int speed = 30;
+
+    public Rigidbody2D ball;
+    
+    // Start is called before the first frame update
     void Start()
     {
-        
+        ball.velocity = new Vector2(-1,-1)*speed;
     }
-
-    // Update is called once per frame
     void FixedUpdate()
     {
         
