777eba58c6c5b9baf3311b39f2197e48b8987f22
AdiWahyuBimantara <18523277@students.uii.ac.id>
2020-03-23 13:09:06 +0700

    commit pertama

---
+++ Assets/BolaMantul.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BolaMantul : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public int speed=20;
+
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(2,2) * speed;
+    }
+
+    // Update is called once per frame
+    void FixidUpdate()
+    {
+     	   
+    }
+}
